bad02ad05ccc056a27aed33be59e95358e785b5f
kroussekax <nvimcpp@proton.me>
2025-03-30 13:08:45 +0700

    Added pos as an argument for adding new meshes

--- include/level.hpp
+++ include/level.hpp
@@ -20,8 +20,15 @@ private:
 	std::vector<std::unique_ptr<Mesh>> meshes; /* prolly gonna be changed after adding assimp support to load models */
 	int current_mesh;
 public:
+	void add_mesh(std::vector<float> vertices, std::vector<unsigned int> indices, glm::vec3 pos){
+		meshes.push_back(std::make_unique<Mesh>(vertices, indices, pos, "res/img/brick.jpg"));
+	}
 	void add_mesh(std::vector<float> vertices, std::vector<unsigned int> indices){
 		meshes.push_back(std::make_unique<Mesh>(vertices, indices, glm::vec3(1.0f, 0.0f, .0f), "res/img/brick.jpg"));
+	}
+
+	void add_mesh(glm::vec3 pos){
+		meshes.push_back(std::make_unique<Mesh>(default_verticecs, default_indices, pos, "res/img/brick.jpg"));
 	}
 	void add_mesh(){
 		meshes.push_back(std::make_unique<Mesh>(default_verticecs, default_indices, glm::vec3(1.0f, 0.0f, .0f), "res/img/brick.jpg"));
