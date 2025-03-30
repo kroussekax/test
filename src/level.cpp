33858f2340344e8cd5e2493ed7ebac709f526661
kroussekax <nvimcpp@proton.me>
2025-03-30 07:30:02 +0700

    fixed new meshes not being drawen properly

--- src/level.cpp
+++ src/level.cpp
@@ -10,8 +10,6 @@
 
 void Level::Draw(){
 	mesh_shader.use();
-	glUniform3f(glGetUniformLocation(mesh_shader.get_id(), "objectColor"), 1.0f, 0.5f, 0.31f);
-	glUniform3f(glGetUniformLocation(mesh_shader.get_id(), "lightColor"), 1.0f, 1.0f, 1.0f);
 
 	for(int i=0;i<meshes.size();i++){
 		if(current_mesh != i)
@@ -22,10 +20,7 @@ void Level::Draw(){
 }
 
 void Level::Draw_UI(){
-	if(InputManager::IsKeyPressed(GLFW_KEY_RIGHT)){
-		current_mesh++;
-		highlight_mesh->get_position() = meshes[current_mesh]->get_position();
-	}
+	if(InputManager::IsKeyPressed(GLFW_KEY_RIGHT)) current_mesh++;
 	if(InputManager::IsKeyPressed(GLFW_KEY_LEFT)) current_mesh--;
 
 	if(current_mesh < 0) current_mesh = 0;
@@ -35,7 +30,7 @@ void Level::Draw_UI(){
 	ImGui::Begin("Mesh Control Panel");
 	{
 		ImGui::Text("Total Mesh: %i", (int)meshes.size());
-		ImGui::Text("Current Mesh: %i", current_mesh);
+		ImGui::Text("Current Mesh: %i", current_mesh+1);
 		ImGui::SliderFloat(std::format("Mesh {0} X", current_mesh).c_str(), &meshes[current_mesh]->get_position().x, -10.0f, 10.0f);
 		ImGui::SliderFloat(std::format("Mesh {0} Y", current_mesh).c_str(), &meshes[current_mesh]->get_position().y, -10.0f, 10.0f);
 		ImGui::SliderFloat(std::format("Mesh {0} Z", current_mesh).c_str(), &meshes[current_mesh]->get_position().z, -10.0f, 10.0f);
@@ -48,7 +43,7 @@ void Level::Draw_UI(){
 
 }
 
-Level::Level(std::vector<float> vertices, std::vector<unsigned int> indices){
+Level::Level(std::vector<float> vertices, std::vector<unsigned int> indices) : default_verticecs{vertices}, default_indices{indices}{
 	mesh_shader = Shader("res/shaders/vertexshaders.glsl", "res/shaders/fragmentshaders.glsl");
 	highlight_shader = Shader("res/shaders/no_texture_vert.glsl", "res/shaders/fraghighlight.glsl");
 
